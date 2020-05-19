d5934a3d0fa3ffbf4ff459b2632787e17e23608b
王健 <wangjian@booyue.com.cn>
2020-05-19 17:35:56 +0800

    4.3智慧早教

---
+++ src/views/Course/CourseMore.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="app">
+		div
+		<v-data></v-data>
+	</div>
+</template>
+
+<script>
+import Data from '@/components/Data.vue';	
+export default {
+	data() {
+		return {};
+	},
+	components: {
+		"v-data":Data
+	}
+};
+</script>
+
+<style lang="less" scoped></style>
